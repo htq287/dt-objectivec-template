84e28bc422b076586cc42d6b0c2a3c2bee6289c1
Hung Q. Truong <htq287@gmail.com>
2019-10-17 11:36:08 +0700

    * add generic template for command line tool

---
+++ CLT_GenericTemplate/CLTProjectName/Sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CLTProjectName
+//
+//  Created by TPLAuthor on 10/17/19.
+//  Copyright © 2019 TPLOrganizationName. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
